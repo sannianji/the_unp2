4afa3e151ecda1953c66527f2e6d4b6e0b71e3a7
sansansan <sannianjisansan@gmail.com>
2018-03-17 15:05:41 +0800

    rwlock

--- pthread_rwlock_rdlock.c
+++ pthread_rwlock_rdlock.c
@@ -1,6 +1,13 @@
 #include "unp.h"
 #include "pthread_rwlock.h"
+static cancelrdwait(void *arg)
+{
+	pthread_rwlock_t *rw;
 
+	rw=arg;
+	rw->rw_nwaitreaders--;
+	pthread_mutex_unlock(&rw->rw_mutex);
+}
 int pthread_rwlock_rdlock(pthread_rwlock_t *rw)
 {
 	int result;
@@ -14,7 +21,9 @@ int pthread_rwlock_rdlock(pthread_rwlock_t *rw)
 	while(rw->rw_refcount<0||rw->rw_nwaitwriters>0)
 	{
 		rw->rw_nwaitreaders++;
+		pthread_cleanup_push(rwlock_cancelrdwait,(void *)rw);
 		result=pthread_cond_wait(&rw->rw_condreaders,&rw->rw_mutex);
+		pthread_cleanup_pop(0);
 		rw->rw_nwaitreaders--;
 		if(result!=0)
 			break;
--- pthread_rwlock_wrlock.c
+++ pthread_rwlock_wrlock.c
@@ -1,6 +1,13 @@
 #include "unp.h"
 #include "pthread_rwlock.h"
-
+static void rwlock_cancelwrwait(void *arg)
+{
+	pthread_rwlock_t *rw;
+	
+	rw=arg;
+	rw->rw_nwaitwriters--;
+	pthread_mutex_unlock(&rw->rw_mutex);
+}
 int pthread_wrlock(pthread_rwlock_t *rw)
 {
 	int result;
@@ -14,7 +21,9 @@ int pthread_wrlock(pthread_rwlock_t *rw)
 	while(rw->rw_refcount!=0)
 	{
 		rw->rw_nwaitwrters++;
+		pthread_cleanup_push(rwlock_cancelwrwait,(void *)rw);
 		result=pthread_cond_wait(&rw_condwrites,&rw_rw_mutex);
+		pthread_cleanup_pop(0);
 		rw->rw_nwaitwrites=--;
 		if(result!=0)
 			break;
