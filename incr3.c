62984046b1b77c25adbc35fc277df0428b79ad83
sannianji <sannianjisansan@gmail.com>
2018-03-25 20:44:59 +0800

    shm

--- incr3.c
+++ incr3.c
@@ -9,7 +9,51 @@ int main(int argc,char **argv)
 {
 	int fd,i,nloop;
 	struct shared *ptr;
-	
+	pid_t pid;
 	if(argc!=3)
-		err_quit("usage : incr3 <pathname>")
+		err_quit("usage : incr3 <pathname> <nloop>");
+	nloop=atoi(argv[2]);
+
+	if((fd=open(argv[1],O_RDWR|O_CREAT,FILE_MODE))<0)
+		err_sys("open");
+	if(write(fd,&shared,sizeof(shared))!=sizeof(shared))
+		err_sys("write");
+	if((ptr=mmap(NULL,sizeof(struct shared),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0))==MAP_FAILED)
+		err_sys("mmap");
+
+	if(close(fd)<0)
+		err_sys("close");
+
+	if(sem_init(&ptr->mutex,1,1)==-1)
+		err_sys("sem_init");
+	
+	setbuf(stdout,NULL);
+
+	if((pid=fork())<0)
+		err_sys("fork");
+	else if(pid==0)
+	{
+		for(i=0;i<nloop;i++)
+		{
+			if(sem_wait(&ptr->mutex)==-1)
+				err_sys("sem_wait");
+			printf("child:%d\n",ptr->count++);
+			if(sem_post(&ptr->mutex)==-1)
+				err_sys("sem_post");
+		}
+	}
+	else if(pid>0)
+	{
+		for(i=0;i<nloop;i++)
+		{
+			if(sem_wait(&ptr->mutex)==-1)
+				err_sys("sem_wait");
+			printf("child:%d\n",ptr->count++);
+			if(sem_post(&ptr->mutex)==-1)
+				err_sys("sem_post");
+		}
+	}
+	exit(0);
+		
+		
 }
